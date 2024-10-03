96bb876daccaa892d066ba3068436e4f7de6e53a
Vishal Jadhav <vsjjadhav1997@gmail.com>
2024-10-03 11:05:26 -0500

    new project created

---
+++ EmployeeProjectGradle/src/main/java/com/example/springgradle/EmployeeProjectGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProjectGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProjectGradleApplication.class, args);
+	}
+
+}
---
+++ EmployeeProjectGradle/src/test/java/com/example/springgradle/EmployeeProjectGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeProjectGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
